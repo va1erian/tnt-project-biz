4900c9767af9c6a8d470c01eee0cbe746e09aa1f
Lowinf <johan.douillard@gmail.com>
2016-02-17 17:23:22 +0100

    Creation of updateUserProfile

--- tnt/src/main/java/com/app5/tnt/ws/user/UserService.java
+++ tnt/src/main/java/com/app5/tnt/ws/user/UserService.java
@@ -1,7 +1,9 @@
 package com.app5.tnt.ws.user;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -9,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.app5.tnt.ws.adapter.DateAdapter;
+import com.app5.tnt.ws.user.jaxb.input.UpdateUserProfileReqInfo;
 import com.app5.tnt.ws.user.jaxb.output.GetUserProfileResInfo;
 
 @Path("/user")
@@ -40,6 +43,30 @@ public class UserService {
 			else {
 				return Response
 						.ok(user, 
+								MediaType.APPLICATION_JSON).build();
+			}
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+			return Response.serverError().entity("Error").build();
+		}
+	}
+	@Path("/updateUserProfile")
+	@PUT
+	@Consumes("application/json")
+	public Response updateUserProfile(@FormParam("updateUser") UpdateUserProfileReqInfo userProfile) {
+		try	{
+			boolean isInDataBase = false;
+			// Check if the user exist in the database
+			if(isInDataBase) {
+				
+				return Response
+						.ok("{update:true}", 
+								MediaType.APPLICATION_JSON).build();
+			}
+			else {
+				return Response
+						.ok("{update:true}", 
 								MediaType.APPLICATION_JSON).build();
 			}
 		}
---
+++ tnt/src/main/java/com/app5/tnt/ws/user/jaxb/input/UpdateUserProfileReqInfo.java
@@ -0,0 +1,85 @@
+package com.app5.tnt.ws.user.jaxb.input;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import com.app5.tnt.ws.adapter.DateAdapter;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "userId",
+	"firstName",
+    "lastName",
+    "birthDate",
+    "gender",
+    "email",
+    "password"
+})
+@XmlRootElement(name="updateUser")
+public class UpdateUserProfileReqInfo implements Serializable {
+	
+	private static final long serialVersionUID = 5028942651663831659L;
+	
+	private String userId;
+	private String firstName;
+	private String lastName;
+	@XmlJavaTypeAdapter(DateAdapter.class)
+	private Date birthDate;
+	private String gender;
+	private String email;
+	private String password;
+	
+	public UpdateUserProfileReqInfo() {
+		
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
