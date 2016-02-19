53023a81d0930109a34c78e0496c73af6039e8b5
Valerian <smiloo@gmail.com>
2016-02-19 23:44:46 +0100

    added log4j and conf

--- src/main/java/com/app5/tnt/ws/login/LoginService.java
+++ src/main/java/com/app5/tnt/ws/login/LoginService.java
@@ -1,8 +1,5 @@
 package com.app5.tnt.ws.login;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -12,34 +9,29 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.app5.tnt.jpa.model.User;
-import com.app5.tnt.jpa.service.Service;
-import com.app5.tnt.utils.MailUtility;
-import com.app5.tnt.utils.NumericUtil;
-import com.app5.tnt.ws.adapter.DateAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.app5.tnt.ws.login.jaxb.LoginUserInfo;
 import com.app5.tnt.ws.login.jaxb.input.AuthentificateReqInfo;
+import com.app5.tnt.ws.login.jaxb.input.NewUserReqInfo;
 import com.app5.tnt.ws.login.jaxb.input.ValidateUserReqInfo;
 import com.app5.tnt.ws.login.jaxb.output.AuthentificateResInfo;
 import com.app5.tnt.ws.login.jaxb.output.UserData;
-import com.app5.tnt.ws.login.jaxb.input.NewUserReqInfo;
 
 @Path("/login")
 public class LoginService {
 	
-	private Service service;
-	private MailUtility mailUtility;;
+	Logger log = LoggerFactory.getLogger(LoginService.class);
 	
-	public LoginService() {
-		//service = new Service();
-		//mailUtility = new MailUtility();
-	}
 	@GET
 	@Produces("application/json")
 	public Response sayHelloPlain( ) {
 		LoginUserInfo logineUserInfo = new LoginUserInfo();
 		logineUserInfo.setNom("SEMGHOUNI");
 		logineUserInfo.setPrenom("Younes");
+		
+		log.info("Hello route");
 		
 		return Response.ok(logineUserInfo, MediaType.APPLICATION_JSON).build();
 	}
--- src/main/java/com/app5/tnt/ws/user/jaxb/input/UpdateUserProfileReqInfo.java
+++ src/main/java/com/app5/tnt/ws/user/jaxb/input/UpdateUserProfileReqInfo.java
@@ -7,9 +7,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import com.app5.tnt.ws.adapter.DateAdapter;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
     "idUser",
