fb84b5b64608a5d0beab70772070cdab017feb45
Nabil <himnabil@gmail.com>
2016-03-15 19:57:19 +0100

    Creating test files

---
+++ src/test/java/com/app5/tnt/ws/address/AddressServiceIntgrationTest.java
@@ -0,0 +1,24 @@
+package com.app5.tnt.ws.address;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddressServiceIntgrationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/app5/tnt/ws/login/LoginServiceIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.app5.tnt.ws.login;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoginServiceIntegrationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testLoginService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAuthentificate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetPassword() {
+		fail("Not yet implemented");
+	}
+
+}
