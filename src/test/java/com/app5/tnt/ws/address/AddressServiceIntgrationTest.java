260e978f2512bb70735226dcd1437df2869d3a83
Nabil <himnabil@gmail.com>
2016-03-19 11:45:43 +0100

    AddressServiceIntegration complete

--- src/test/java/com/app5/tnt/ws/address/AddressServiceIntgrationTest.java
+++ src/test/java/com/app5/tnt/ws/address/AddressServiceIntgrationTest.java
@@ -25,12 +25,52 @@ public class AddressServiceIntgrationTest {
 	}
 	
 	@Test
-	public void testList_UserExqist() {
+	public void testList_UserExqis_manyAddresses() {
 		List<Address> expected = new ArrayList<Address>();
 		expected.add(DBInit.a1);
 		expected.add(DBInit.a3);
 		expected.add(DBInit.a4);
 		String input = DBInit.u1.getId()+"";
+		AddressService service = new AddressService();
+		Response r = service.list(input);
+		List<AddressData> result = ((ListAddressResInfo) r.getEntity()).getDataAddressList();	
+		
+		assertEquals(200, r.getStatus());
+		assertEquals(expected.size(), result.size());
+		
+		for ( Address a : expected){
+			boolean exsist = false; 
+			for( AddressData ad : result ){
+				boolean matche = true ;
+				matche = matche && (a.getId().longValue() == ad.getIdAddress().longValue());
+				matche = matche && (a.getGpsLatitude().doubleValue()	== ad.getGpsLatitude().doubleValue());
+				matche = matche && (a.getGpsLongitude().doubleValue()	== ad.getGpsLongitude().doubleValue());
+				matche = matche && (a.getFormatedAddress().equals(ad.getFormattedAddress()));
+				exsist = exsist || matche;
+			}
+			assertTrue("->" + a.getId() + ':' + a.getFormatedAddress(), exsist);
+		}		
+	}
+	
+	@Test
+	public void testList_UserExqist_noAddress() {
+		
+		String input = DBInit.u2.getId()+"";
+		AddressService service = new AddressService();
+		Response r = service.list(input);
+		List<AddressData> result = ((ListAddressResInfo) r.getEntity()).getDataAddressList();	
+		
+		assertEquals(200, r.getStatus());
+		assertEquals(0, result.size());
+				
+	}
+	
+	@Test
+	public void testList_UserExqist_oneAddresse() {
+		List<Address> expected = new ArrayList<Address>();
+		expected.add(DBInit.a5);
+		
+		String input = DBInit.u3.getId()+"";
 		AddressService service = new AddressService();
 		Response r = service.list(input);
 		List<AddressData> result = ((ListAddressResInfo) r.getEntity()).getDataAddressList();	
